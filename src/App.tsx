959af16b32f214ba586520f42f255e523cbc76ef
Dead End <dead.end@inbox.ru>
2025-02-22 22:19:39 +0100

    using the provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { ConfigContext } from './components/ConfigContext';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import { Admin } from './pages/Admin';
 import Navigation from './components/Navigation';
 import Container from '@mui/material/Container';
-import { useConfig } from './libs/hooks/useConfig';
 import BookList from './pages/book/BookList';
 import BookCreate from './pages/book/BookCreate';
+import { GithubConfigProvider } from './libs/hooks/GithubConfigProvider';
 
 function App() {
-  const config = useConfig();
-
   return (
     <Container>
-      <ConfigContext.Provider value={config}>
-        {config.config.user !== '' ? (
-          <BrowserRouter>
-            <Navigation />
-            <Routes>
-              <Route path='trainer'>
-                <Route index element={<Home />} />
-                <Route path='admin' element={<Admin />} />
-                <Route path='books/create' element={<BookCreate />} />
-                <Route path='books' element={<BookList />}></Route>
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        ) : (
-          <Admin />
-        )}
-      </ConfigContext.Provider>
+      <GithubConfigProvider>
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route path='trainer'>
+              <Route index element={<Home />} />
+              <Route path='admin' element={<Admin />} />
+              <Route path='books/create' element={<BookCreate />} />
+              <Route path='books' element={<BookList />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GithubConfigProvider>
     </Container>
   );
 }
