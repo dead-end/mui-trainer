f4d1cda2f8d8fa2848eba8c1d90251ec1988380d
Dead End <dead.end@inbox.ru>
2025-02-26 23:33:05 +0100

    added return value

--- src/libs/utils/validation.ts
+++ src/libs/utils/validation.ts
@@ -4,7 +4,8 @@ export const validateEmpty = (
 ) => {
   if (value === '') {
     setError('Please enter value!');
-  } else {
-    setError('');
+    return false;
   }
+  setError('');
+  return true;
 };
