113810c8bcff620e552c57fd1b574b4c2570658b
Dead End <dead.end@inbox.ru>
2025-03-09 11:35:45 +0100

    new routes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import BookCreate from './pages/book/BookCreate';
 import { GithubConfigProvider } from './libs/hooks/github/GithubConfigProvider';
 import BookUpdate from './pages/book/BookUpdate';
 import { ErrorProvider } from './libs/hooks/error/ErrorProvider';
+import ChapterList from './pages/chapter/ChapterList';
+import ChapterCreate from './pages/chapter/ChapterCreate';
+import ChapterUpdate from './pages/chapter/ChapterUpdate';
 
 function App() {
   return (
@@ -25,6 +28,18 @@ function App() {
                 <Route path='books/create' element={<BookCreate />} />
                 <Route path='books/update/:id' element={<BookUpdate />} />
                 <Route path='books' element={<BookList />}></Route>
+                <Route
+                  path='book/:bookId/create'
+                  element={<ChapterCreate />}
+                ></Route>
+                <Route
+                  path='book/:bookId/update/:id'
+                  element={<ChapterUpdate />}
+                ></Route>
+                <Route
+                  path='book/:bookId/chapters'
+                  element={<ChapterList />}
+                ></Route>
               </Route>
             </Routes>
           </BrowserRouter>
