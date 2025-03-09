5952e4c8d72db34610d267732d478c4b42fe3bb1
Dead End <dead.end@inbox.ru>
2025-03-09 11:28:11 +0100

    update

--- src/pages/book/BookList.tsx
+++ src/pages/book/BookList.tsx
@@ -1,7 +1,3 @@
-import { useEffect, useState } from 'react';
-import { TBook } from '../../libs/types';
-import { bookDelete, bookListing } from '../../libs/model/book';
-import { Link, useNavigate } from 'react-router';
 import TableContainer from '@mui/material/TableContainer';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
@@ -14,19 +10,27 @@ import EditIcon from '@mui/icons-material/Edit';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
-import { useGithubConfig } from '../../libs/hooks/github/useGithubConfig';
-import { useError } from '../../libs/hooks/error/useError';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import Typography from '@mui/material/Typography';
+
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+
+import { TBook } from '../../libs/types';
+import { bookDelete, bookListing } from '../../libs/model/book';
+import { toChapterList } from '../../libs/model/chapter';
+import Confirmation from '../../components/Confirmation';
+import { useGithubConfig } from '../../libs/hooks/github/useGithubConfig';
+import { useError } from '../../libs/hooks/error/useError';
 
 const BookList = () => {
   const navigate = useNavigate();
   const { addError } = useError();
   const githubConfig = useGithubConfig();
   const [books, setBooks] = useState<TBook[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [deleteId, setDeleteId] = useState<string | undefined>();
 
   useEffect(() => {
     const load = async () => {
@@ -36,16 +40,13 @@ const BookList = () => {
       } else {
         addError(result.getMessage());
       }
-      setLoading(false);
     };
 
     load();
   }, []);
 
-  const onDelete = async (id: string) => {
-    setLoading(true);
-    const result = await bookDelete(githubConfig, id);
-    setLoading(false);
+  const onDelete = async (value: string) => {
+    const result = await bookDelete(githubConfig, value);
 
     if (result.hasError()) {
       addError(result.getMessage());
@@ -60,84 +61,92 @@ const BookList = () => {
   };
 
   const onShow = (id: string) => {
-    navigate(`/trainer/book/${id}/chapters`);
+    navigate(toChapterList(id));
   };
 
   return (
-    <Paper sx={{ paddingX: 8, paddingY: 4 }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 4,
-        }}
-      >
-        <Typography variant='h6'>Book List</Typography>
-        <TableContainer>
-          <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-            <TableHead color='primary'>
-              <TableRow hover>
-                <TableCell>Id</TableCell>
-                <TableCell>Title</TableCell>
-                <TableCell>Description</TableCell>
-                <TableCell>Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {books.map((book) => (
-                <TableRow hover key={book.id}>
-                  <TableCell>{book.id}</TableCell>
-                  <TableCell>{book.title}</TableCell>
-                  <TableCell>{book.description}</TableCell>
-                  <TableCell>
-                    <Tooltip title={`Edit book: ${book.id}`}>
-                      <IconButton
-                        color='primary'
-                        loading={loading}
-                        onClick={() => onUpdate(book.id)}
-                      >
-                        <EditIcon />
-                      </IconButton>
-                    </Tooltip>
-                    <Tooltip title={`Delete book: ${book.id}`}>
-                      <IconButton
-                        color='primary'
-                        loading={loading}
-                        onClick={() => onDelete(book.id)}
-                      >
-                        <HighlightOffIcon />
-                      </IconButton>
-                    </Tooltip>
-                    <Tooltip title={`Show chapters: ${book.id}`}>
-                      <IconButton
-                        color='primary'
-                        loading={loading}
-                        onClick={() => onShow(book.id)}
-                      >
-                        <FormatListBulletedIcon />
-                      </IconButton>
-                    </Tooltip>
-                  </TableCell>
+    <>
+      <Paper sx={{ paddingX: 8, paddingY: 4 }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 4,
+          }}
+        >
+          <Typography variant='h6'>Book List</Typography>
+          <TableContainer>
+            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+              <TableHead color='primary'>
+                <TableRow hover>
+                  <TableCell>Id</TableCell>
+                  <TableCell>Title</TableCell>
+                  <TableCell>Description</TableCell>
+                  <TableCell>Actions</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Stack direction={'row'} gap={3}>
-          <Button variant='contained' component={Link} to='/trainer/'>
-            Cancel
-          </Button>
-          <Button
-            loading={loading}
-            variant='contained'
-            component={Link}
-            to='/trainer/books/create'
-          >
-            Create
-          </Button>
-        </Stack>
-      </Box>
-    </Paper>
+              </TableHead>
+              <TableBody>
+                {books.map((book) => (
+                  <TableRow hover key={book.id}>
+                    <TableCell>{book.id}</TableCell>
+                    <TableCell>{book.title}</TableCell>
+                    <TableCell>{book.description}</TableCell>
+                    <TableCell>
+                      <Tooltip title={`Delete book: ${book.id}`}>
+                        <IconButton
+                          color='primary'
+                          onClick={() => {
+                            setDeleteId(book.id);
+                          }}
+                        >
+                          <HighlightOffIcon />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title={`Edit book: ${book.id}`}>
+                        <IconButton
+                          color='primary'
+                          onClick={() => onUpdate(book.id)}
+                        >
+                          <EditIcon />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title={`Show chapters: ${book.id}`}>
+                        <IconButton
+                          color='primary'
+                          onClick={() => onShow(book.id)}
+                        >
+                          <FormatListBulletedIcon />
+                        </IconButton>
+                      </Tooltip>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Stack direction={'row'} gap={3}>
+            <Button variant='contained' component={Link} to='/trainer/'>
+              Cancel
+            </Button>
+            <Button
+              variant='contained'
+              component={Link}
+              to='/trainer/books/create'
+            >
+              Create
+            </Button>
+          </Stack>
+        </Box>
+      </Paper>
+
+      <Confirmation<string>
+        title='Delete Book'
+        message={`Do you want to delete the book: ${deleteId}?`}
+        confirmValue={deleteId}
+        setConfirmValue={setDeleteId}
+        confirmFct={onDelete}
+      />
+    </>
   );
 };
 
